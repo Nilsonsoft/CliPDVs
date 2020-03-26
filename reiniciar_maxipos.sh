7b907f9c5f363b090a25f30f082021c84aa47234
Nilsonlinux <nilsonlinux@gmail.com>
2020-03-25 21:44:50 -0300

    Add files via upload

--- reiniciar_maxipos.sh
+++ reiniciar_maxipos.sh
@@ -69,5 +69,5 @@ sair) echo -e "\033[1;33m Você fechou o (CliPDV's). Até a próxima!!!\033[0m"
 exit;;
 *) echo "Terminal inexistente. Verifique se você digitou corretamente."
 sleep 2
-./CliPDVs/reiniciar_maxipos/reiniciar_maxipos.sh;;
+./CliPDVs/reiniciar_maxipos.sh;;
 esac
