5c22d2b95e7d1e3c9b0f965b8accf2c5b5e046fb
Nilsonlinux <nilsonlinux@gmail.com>
2020-03-25 22:13:33 -0300

    Add files via upload

--- outros/ids_balanca.sh
+++ outros/ids_balanca.sh
@@ -7,18 +7,22 @@ echo -e "\033[0;32m |     | |   | | | |_) | | | \ \ / / __|         |\033[0m"
 echo -e "\033[0;32m |     | |___| | |  __/| |_| |\ V /\__ \         |\033[0m"
 echo -e "\033[0;32m |      \____|_|_|_|   |____/  \_/ |___/ © 2020  |\033[0m"
 echo -e "\033[0;32m | BETA                                          |\033[0m"
-echo -e "\033[0;32m |-----------[ Atualização (CliPDVs) ]-----------|\033[0m"
-echo -e "\033[0;33m Atualizando (CliPDVs). Por favor aguarde . . .\033[0m" 
-sleep 2
-rm -rf CliPDVs_beta
-sleep 3
-echo -e "\033[0;33m(CliPDVs) está sendo atualizado e será executado logo após . . .\033[0m" 
-sleep 3
-git clone https://github.com/nilsonlinux/CliPDVs.git && cd ./CliPDVs && chmod +x * && cd ../
-sleep 2
-echo -e "\033[0;33m(CliPDVs) Atualizado com sucesso. Inicializando...\033[0m" 
-sleep 3
+echo -e "\033[0;32m |------[ Identificação das Balanças SM-03 ]-----|\033[0m"
+echo -e "\033[0;33m |                                               |\033[0m"
+echo -e "\033[0;33m |21 - Frios                                     |\033[0m"
+echo -e "\033[0;33m |24 - Frios                                     |\033[0m" 
+echo -e "\033[0;33m |29 - Frios                                     | \033[0m" 
+echo -e "\033[0;33m |25 - Padaria                                   | \033[0m" 
+echo -e "\033[0;33m |26 - Padaria                                   | \033[0m" 
+echo -e "\033[0;33m |22 - Peixaria                                  | \033[0m" 
+echo -e "\033[0;33m |23 - Hortifruti                                | \033[0m" 
+echo -e "\033[0;33m |21 - Frios                                     | \033[0m" 
+echo -e "\033[0;33m |21 - Frios                                     | \033[0m" 
+echo -e "\033[0;33m |21 - Frios                                     |\033[0m" 
+echo -e "\033[0;33m |                                               |" 
+echo -e "\033[0;33m |Digite 0 para voltar                           |\033[0m" 
+read opcion
+case $opcion in
+0) ./CliPDVs/CliPDVs.sh;;
 clear &&
-./CliPDVs/CliPDVs.sh
-sleep 2
 # © 2020 Nilsonlinux
