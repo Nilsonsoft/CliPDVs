104a3fcdca2dc68d65dc5e8c1b37e540a6b44100
Nilsonlinux <nilsonlinux@gmail.com>
2020-03-26 20:51:31 -0300

    Add files via upload

--- outros/ramais_ti.sh
+++ outros/ramais_ti.sh
@@ -29,14 +29,13 @@ echo -e "\033[0;32m |-----------------------------------------------|\033[0m"
 echo -e "\033[0;32m |            \033[0;33m        HARDWARE                 \033[0m \033[0;32m | \033[0m"
 echo -e "\033[0;32m |   \033[1;32m 3451 - 3505 - 3450 - 3696 - 9886 - 9885 \033[0m  \033[0;32m | \033[0m"
 echo -e "\033[0;32m |-----------------------------------------------|\033[0m"
-echo -e "\033[0;32m |  \033[0m \033[0;32mDigite\033[0;32m \033[0;36mvoltar\033[0m \033[0;32m Para ir para o menu inicial \033[0;32m | \033[0m"
+echo -e "\033[0;32m | \033[0m \033[0;32mDigite\033[0m \033[0;36m 0 \033[0m \033[0;32m Para voltar pro menu principal \033[0m \033[0;32m | \033[0m"
 echo -e "\033[0;32m |-----------------------------------------------|\033[0m"
 echo -e "\033[0;32m |/ \033[0;33mDIGITE A OPÇÃO \033[0m\033[0;32m\_____________\033[0;36mby:Nilsonlinux\033[0;32m__|\033[0m"
 read opcion
 case $opcion in
 voltar) ./CliPDVs/CliPDVs.sh;;
-0) echo -e "\033[1;33m Você saiu do (CliPDV's). Até a próxima!!!\033[0m" 
-exit;;
+0) ./CliPDVs/CliPDVs.sh;;
 sair) echo -e "\033[1;33m Você fechou o (CliPDV's). Até a próxima!!!\033[0m"
 exit;;
 *) echo "Opção inexistente. Verifique se você digitou corretamente."
