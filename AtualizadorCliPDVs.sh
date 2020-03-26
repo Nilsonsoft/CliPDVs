8fe1623fd3a32e80c36e6636217cd253dbfe9779
Nilsonlinux <nilsonlinux@gmail.com>
2020-03-25 22:14:41 -0300

    Add files via upload

--- AtualizadorCliPDVs.sh
+++ AtualizadorCliPDVs.sh
@@ -10,7 +10,7 @@ echo -e "\033[0;32m | BETA                                          |\033[0m"
 echo -e "\033[0;32m |-----------[ Atualização (CliPDVs) ]-----------|\033[0m"
 echo -e "\033[0;33m Atualizando (CliPDVs). Por favor aguarde . . .\033[0m" 
 sleep 2
-rm -rf CliPDVs_beta
+rm -rf CliPDVs
 sleep 3
 echo -e "\033[0;33m(CliPDVs) está sendo atualizado e será executado logo após . . .\033[0m" 
 sleep 3
