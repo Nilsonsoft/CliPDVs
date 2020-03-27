30bb0d674f9beaf472a8ca37510212d51299ea7e
Nilsonlinux <nilsonlinux@gmail.com>
2020-03-27 18:14:49 -0300

    Update CliPDVs.sh

--- CliPDVs.sh
+++ CliPDVs.sh
@@ -18,7 +18,7 @@ echo -e "\033[0;32m |----[\033[0;33m 7 \033[0m\033[0;32m]\033[0m\033[0m \033[1;3
 echo -e "\033[0;32m |-----------------------------------------------|\033[0m"
 echo -e "\033[0;32m |---------[\033[0;33m 8 \033[0m\033[0;32m]\033[0m\033[0m \033[1;32mAtualizar CliPDVs\033[0m  \033[0;32m             | \033[0m"
 echo -e "\033[0;32m |-----------------------------------------------|\033[0m"
-echo -e "\033[0;32m |----------[\033[0;33m 0 \033[0m\033[0;32m]\033[0m\033[0m \033[1;32m Fechar o Script  \033[0m \033[0;32m     | \033[0m"
+echo -e "\033[0;32m |----------[\033[0;33m 0 \033[0m\033[0;32m]\033[0m\033[0m \033[1;32m Fechar o Script  \033[0m \033[0;32m            | \033[0m"
 echo -e "\033[0;32m |-----------------------------------------------|\033[0m"
 echo -e "\033[0;32m |/ \033[0;33mDIGITE A OPÇÃO \033[0m\033[0;32m\_____________\033[0;36mby:Nilsonlinux\033[0;32m__|\033[0m"
 read opcion
@@ -32,7 +32,7 @@ case $opcion in
 7) ./CliPDVs/outros/sobre.sh;;
 8) ./CliPDVs/AtualizadorCliPDVs.sh;;
 0) echo -e "\033[1;32m Você fechou o script. Até a próxima!!!\033[0m" 
-exit;;
+exit
 *) echo "Opção inexistente. Verifique se você digitou corretamente."
 sleep 2
 ./CliPDVs/CliPDVs.sh;
