0ff565cb32aaffaffc5e24ad54c1d1cff1e48fce
Nilsonlinux <nilsonlinux@gmail.com>
2020-03-27 11:43:20 -0300

    Update reiniciar_maxipos.sh

--- reiniciar_maxipos.sh
+++ reiniciar_maxipos.sh
@@ -31,7 +31,7 @@ echo -e "\033[1;33m[126]\033[0m - Caixa 21 - \033[0;36mIP\033[0m ->\033[0;32m 19
 echo -e "\033[1;33m[128]\033[0m - \033[0;34mBoteco\033[0m - \033[0;36mIP\033[0m ->\033[0;32m 192.168.3.129 \033[0m"
 echo -e "--------------------------------------------"
 echo -e "Digite \033[0;31m'todos'\033[0m sem as aspas, para reiniciar o MaxiPOS de todos os terminais"
-echo -e "Digite \033[0;32m'voltar'\033[0m sem as aspas, para voltar para o menu inicial."
+echo -e "Digite \033[0;32m'0'\033[0m sem as aspas, para voltar para o menu inicial."
 echo -e "Digite \033[0;32m'sair'\033[0m sem as aspas, para fechar o script"
 echo -e "--------------------------------------------"
 echo -e "DIGITE O NÚMERO DO PDV. EX: \033[0;31m05\033[0m \033[1;36mpara reiniciar o MaxiPOS do caixa 05\033[0;32m."
