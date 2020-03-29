7eb6458328a0203f7f6775405ae38e450e02810f
Nilsonlinux <nilsonlinux@gmail.com>
2020-03-28 22:23:59 -0300

    Add files via upload

--- AtualizadorCliPDVs.sh
+++ AtualizadorCliPDVs.sh
@@ -8,16 +8,14 @@ echo -e "\033[0;32m |     | |___| | |  __/| |_| |\ V /\__ \         |\033[0m"
 echo -e "\033[0;32m |      \____|_|_|_|   |____/  \_/ |___/ © 2020  |\033[0m"
 echo -e "\033[0;32m | BETA                                          |\033[0m"
 echo -e "\033[0;32m |-----------[ Atualização (CliPDVs) ]-----------|\033[0m"
-echo -e "\033[0;33m Atualizando (CliPDVs). Por favor aguarde . . .\033[0m" 
-sleep 2
-rm -rf CliPDVs
+echo -e "\033[0;33m Atualizando (CliPDVs). Por favor aguarde . . .\033[0m"
 sleep 3
-echo -e "\033[0;33m(CliPDVs) está sendo atualizado e será executado logo após . . .\033[0m" 
+echo -e "\033[0;33m(CliPDVs) está sendo atualizado e será executado logo após . . .\033[0m"
 sleep 3
 rm -rf CliPDVs && git clone https://github.com/nilsonlinux/CliPDVs.git && chmod -R 777 ./CliPDVs && clear && ./CliPDVs/CliPDVs.sh
 sleep 2
-echo -e "\033[0;33m(CliPDVs) Atualizado com sucesso. Inicializando...\033[0m" 
-sleep 3
+echo -e "\033[0;33m(CliPDVs) Atualizado com sucesso. Inicializando...\033[0m"
+sleep 2
 clear
 ./CliPDVs/CliPDVs.sh
 # © 2020 Nilsonlinux
