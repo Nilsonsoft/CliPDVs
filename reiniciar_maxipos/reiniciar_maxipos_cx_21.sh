7265c30b00c4c2471f17c572721d7d3d89aff394
Nilsonlinux <nilsonlinux@gmail.com>
2020-03-30 19:07:47 -0300

    Update reiniciar_maxipos_cx_21.sh

--- reiniciar_maxipos/reiniciar_maxipos_cx_21.sh
+++ reiniciar_maxipos/reiniciar_maxipos_cx_21.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "################################################################"
-echo -e "Terminal [122] - Eletro 1 - IP -\033[0;34m 192.168.3.55 \033[0m"
-sshpass -p 1 ssh -o "StrictHostKeyChecking no" root@192.168.3.55 "it-restart-application.sh";
+echo -e "Terminal [126] - Caixa 21 - IP -\033[0;34m 192.168.3.119 \033[0m"
+sshpass -p 1 ssh -o "StrictHostKeyChecking no" root@192.168.3.119 "it-restart-application.sh";
 echo "MaxiPOS reiniciado...";
 sleep 2
 ./CliPDVs/reiniciar_maxipos.sh;
