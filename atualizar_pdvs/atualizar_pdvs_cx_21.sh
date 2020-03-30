3993e644ab58a93985d2184886b3d4a913ee2b49
Nilsonlinux <nilsonlinux@gmail.com>
2020-03-30 19:04:25 -0300

    Update atualizar_pdvs_cx_21.sh

--- atualizar_pdvs/atualizar_pdvs_cx_21.sh
+++ atualizar_pdvs/atualizar_pdvs_cx_21.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "################################################################"
-echo -e "Terminal [122] - Eletro 1 - IP -\033[0;34m 192.168.3.55 \033[0m"
-sshpass -p 1 ssh -o "StrictHostKeyChecking no" root@192.168.3.55 "it-update-pdv.sh";
+echo -e "Terminal [126] - Caica 21 - IP -\033[0;34m 192.168.3.119 \033[0m"
+sshpass -p 1 ssh -o "StrictHostKeyChecking no" root@192.168.3.119 "it-update-pdv.sh";
 echo "Terminal atualizado... Por favor verifique.";
 sleep 2
 ./CliPDVs/atualizar_pdvs.sh;
