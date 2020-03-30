bb601cd4286c9a4c9aa6c430d38e97034a63ed0f
Nilsonlinux <nilsonlinux@gmail.com>
2020-03-30 19:01:08 -0300

    Update atualizar_imagem_cx_21.sh

--- atualizar_imagem/atualizar_imagem_cx_21.sh
+++ atualizar_imagem/atualizar_imagem_cx_21.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "################################################################"
-echo -e "Terminal [122] - Eletro 1 - IP -\033[0;34m 192.168.3.55 \033[0m"
-sshpass -p 1 ssh -o "StrictHostKeyChecking no" root@192.168.3.55 "it-update-imagens.sh";
+echo -e "Terminal [126] - Caixa 21 - IP -\033[0;34m 192.168.3.119 \033[0m"
+sshpass -p 1 ssh -o "StrictHostKeyChecking no" root@192.168.3.119 "it-update-imagens.sh";
 echo "Imagem atualizada... Por favor verifique.";
 sleep 2
 ./CliPDVs/atualizar_imagem.sh;
