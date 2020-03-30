9fb74db2c385a4ac9f8b6674e1864483f30d155c
Nilsonlinux <nilsonlinux@gmail.com>
2020-03-30 10:13:01 -0300

    Add files via upload

--- atualizar_pdvs/atualizar_pdvs_res.sh
+++ atualizar_pdvs/atualizar_pdvs_res.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "################################################################"
-echo -e "Terminal [127] - Boteco - IP -\033[0;34m 192.168.3.138 \033[0m"
+echo -e "Terminal [127] - PDV Reserva - IP -\033[0;34m 192.168.3.138 \033[0m"
 sshpass -p 1 ssh -o "StrictHostKeyChecking no" root@192.168.3.138 "it-update-pdv.sh";
 echo "Terminal atualizado... Por favor verifique.";
 sleep 2
